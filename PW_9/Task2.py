16408af5332f118b1be70900a4475116635f4d02
ivasx <ambroziak.v.ivan@gmail.com>
2025-06-06 17:34:29 +0300

    function v(x,y)

---
+++ PW_9/Task2.py
@@ -0,0 +1,28 @@
+import math
+
+def input_arg():
+    try:
+        x = float(input("Введіть значення x: "))
+        y = float(input("Введіть значення y: "))
+        return x, y
+    except ValueError:
+        print('Введено некоректне значення одгого з елементів, спробуйте знову.')
+        input_arg()
+        return None
+
+
+def u_function(x, y):
+    return x**2 + y**2
+
+def v_function(u):
+    return (1 + u**2) * math.log(u)**2
+
+if __name__ == '__main__':
+    x, y = input_arg()
+    u = u_function(x, y)
+    try:
+        v = v_function(u)
+        print(f"u = {u}")
+        print(f"v = {v}")
+    except ValueError as error:
+        print(f"Помилка: {error}")
