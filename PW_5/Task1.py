1eff5a1ab6d3d4c8ecdd26b44a2c7fadac22b992
ivasx <ambroziak.v.ivan@gmail.com>
2025-06-16 22:51:17 +0300

    fixed product

--- PW_5/Task1.py
+++ PW_5/Task1.py
@@ -8,18 +8,18 @@ for n in input('Введіть числа через пробіл: ').split():
 positives = [n for n in nums_inp if n >= 0]
 negatives = [n for n in nums_inp if n < 0]
 
-
 min_index = nums_inp.index(min(nums_inp))
 max_index = nums_inp.index(max(nums_inp))
 
 s, e = sorted([max_index, min_index])
-if e - s > 0:
+if e - s > 1:
     product = 1
     for i in nums_inp[s + 1 :e]:
         product *= i
 else:
     product = 0
+
 print(f'Масив: {nums_inp}')
 print(f'Сума від\'ємних елементів масиву: {sum(negatives)}')
-print(f'Добуток елементів масиву, розташованих між максимальним ({max(nums_inp)})n'
+print(f'Добуток елементів масиву, розташованих між максимальним ({max(nums_inp)}) '
       f'і мінімальним ({min(nums_inp)}) елементами: {product}')
